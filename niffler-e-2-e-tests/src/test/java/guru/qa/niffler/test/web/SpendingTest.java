321b950981017c7d82eb03ffb15817a63e019f18
YegorRysh <yegor.ryshkevich@gmail.com>
2025-04-01 16:09:05 +0300

    Sync with main branch

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -2,9 +2,9 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.BrowserExtension;
 import guru.qa.niffler.jupiter.Category;
-import guru.qa.niffler.jupiter.Spend;
+import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
