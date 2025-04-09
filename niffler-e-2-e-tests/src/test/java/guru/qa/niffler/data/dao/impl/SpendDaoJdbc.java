9573022df479f212b0da96d39c8c9bfd10439248
dtuchs <dtuchs@gmail.com>
2025-04-09 12:00:14 +0300

    stream #4.2 -  JDBC API + JTA (Atomicos)
    (cherry picked from commit 5257484afa12c5e9fa8d8652f7dc6f06dbf2d47d)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
@@ -16,34 +16,38 @@ public class SpendDaoJdbc implements SpendDao {
 
   private static final Config CFG = Config.getInstance();
 
+  private final Connection connection;
+
+  public SpendDaoJdbc(Connection connection) {
+    this.connection = connection;
+  }
+
   @Override
   public SpendEntity create(SpendEntity spend) {
-    try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
-      try (PreparedStatement ps = connection.prepareStatement(
-              "INSERT INTO spend (username, spend_date, currency, amount, description, category_id) " +
-                      "VALUES ( ?, ?, ?, ?, ?, ?)",
-              Statement.RETURN_GENERATED_KEYS
-      )) {
-        ps.setString(1, spend.getUsername());
-        ps.setDate(2, new Date(spend.getSpendDate().getTime()));
-        ps.setString(3, spend.getCurrency().name());
-        ps.setDouble(4, spend.getAmount());
-        ps.setString(5, spend.getDescription());
-        ps.setObject(6, spend.getCategory().getId());
+    try (PreparedStatement ps = connection.prepareStatement(
+        "INSERT INTO spend (username, spend_date, currency, amount, description, category_id) " +
+            "VALUES ( ?, ?, ?, ?, ?, ?)",
+        Statement.RETURN_GENERATED_KEYS
+    )) {
+      ps.setString(1, spend.getUsername());
+      ps.setDate(2, spend.getSpendDate());
+      ps.setString(3, spend.getCurrency().name());
+      ps.setDouble(4, spend.getAmount());
+      ps.setString(5, spend.getDescription());
+      ps.setObject(6, spend.getCategory().getId());
 
-        ps.executeUpdate();
+      ps.executeUpdate();
 
-        final UUID generatedKey;
-        try (ResultSet rs = ps.getGeneratedKeys()) {
-          if (rs.next()) {
-            generatedKey = rs.getObject("id", UUID.class);
-          } else {
-            throw new SQLException("Can`t find id in ResultSet");
-          }
+      final UUID generatedKey;
+      try (ResultSet rs = ps.getGeneratedKeys()) {
+        if (rs.next()) {
+          generatedKey = rs.getObject("id", UUID.class);
+        } else {
+          throw new SQLException("Can`t find id in ResultSet");
         }
-        spend.setId(generatedKey);
-        return spend;
       }
+      spend.setId(generatedKey);
+      return spend;
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
