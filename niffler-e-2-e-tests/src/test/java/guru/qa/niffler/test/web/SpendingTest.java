a69919983b6a0f21be0488c79e279b0bd3efa854
YegorRysh <yegor.ryshkevich@gmail.com>
2025-05-13 10:22:22 +0300

    stream #9.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -1,19 +1,31 @@
 package guru.qa.niffler.test.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spend;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebElement;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 @WebTest
 public class SpendingTest {
@@ -36,6 +48,27 @@ public class SpendingTest {
         .editDescription(newDescription);
 
     new MainPage().checkThatTableContains(newDescription);
+  }
+
+  @User(
+          spendings = @Spend(
+                  category = "Обучение",
+                  description = "Обучение Advanced 2.0",
+                  amount = 79990,
+                  currency = CurrencyValues.RUB
+          )
+  )
+  @ScreenShotTest("img/expected-stat.png")
+  void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException, InterruptedException {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+            .doLogin(user.username(), user.testData().password());
+    Thread.sleep(3000);
+
+    BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+    assertFalse(new ScreenDiffResult(
+            expected,
+            actual
+    ));
   }
 
 }
